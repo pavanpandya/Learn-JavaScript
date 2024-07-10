e0d8134984d470deda391d476af10beb55234e35
pavanpandya <pavanpandya1311@gmail.com>
2024-07-09 20:24:40 -0400

    Lecture on intro to js

--- 00_Hello.js
+++ 00_Hello.js
@@ -1,4 +1,71 @@
+// ----------------------------------------------
+// Documentation = "https://tc39.es/ecma262/" and "https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+// ----------------------------------------------
+
 console.log("Hello from Pavan!")
 
+// Code after double slashes // or between /* and */ is treated as a comment.
 // How to run the JS file? (Standalone without using index.html and adding script in it)
 // node filename.js
+
+// ----------------------------------------------
+// Introduction to JS: (Reference: https://www.w3schools.com/js/default.asp)
+// ----------------------------------------------
+
+
+// The JavaScript syntax defines two types of values:
+// 1. Fixed values = Fixed values are called Literals.
+// 2. Variable values = Variable values are called Variables.
+
+// JavaScript Literals
+// The two most important syntax rules for fixed values are:
+// 1. Numbers are written with or without decimals. Eg: 10.50, 1001
+// 2. Strings are text, written within double or single quotes. Eg: "Hello", 'Hello'
+
+// JavaScript Variables
+// In a programming language, variables are used to store data values.
+// JavaScript uses the keywords var, let and const to declare variables.
+// An equal sign is used to assign values to variables.
+// Eg: var x = 5;
+
+// JavaScript Operators
+// JavaScript uses arithmetic operators (+, -, *, /) to compute values.
+
+// JavaScript Keywords
+// JavaScript keywords are used to identify actions to be performed.
+
+// The let keyword tells the browser to create variables:
+// let x = 5;
+// let y = 6;
+// let z = x + y;
+
+// The var keyword also tells the browser to create variables:
+// var x, y;
+// x = 5 + 6;
+// y = x * 10;
+
+// NOTE:
+// The var keyword was used in all JavaScript code from 1995 to 2015.
+// The let and const keywords were added to JavaScript in 2015.
+// The var keyword should only be used in code written for older browsers.
+
+// When to Use var, let, or const?
+// 1. Always declare variables
+// 2. Always use const if the value should not be changed
+// 3. Always use const if the type should not be changed (Arrays and Objects)
+// 4. Only use let if you can't use const
+// 5. Only use var if you MUST support old browsers.
+
+// JavaScript Identifiers / Names
+// Identifiers are JavaScript names.
+// Identifiers are used to name variables and keywords, and functions.
+// The rules for legal names are the same in most programming languages.
+
+// A JavaScript name must begin with:
+
+// 1. A letter (A-Z or a-z)
+// 2. A dollar sign ($)
+// 2. Or an underscore (_)
+// 3. Subsequent characters may be letters, digits, underscores, or dollar signs.
+
+// Note: Numbers are not allowed as the first character in names.
